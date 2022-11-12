7704f47701ceadaa67d9a65d94d385b726cd236c
Andrey <ajedrezandrey@gmail.com>
2022-11-12 16:17:27 -0500

    Barra de navegación y Pie de pagiina Con en rutamiento

--- Frontend/FrontendAccesorios/src/app/app-routing.module.ts
+++ Frontend/FrontendAccesorios/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "inicio",
+    component: InicioComponent
+  }, 
+  {
+    path: '**',
+    component: ErrorComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
