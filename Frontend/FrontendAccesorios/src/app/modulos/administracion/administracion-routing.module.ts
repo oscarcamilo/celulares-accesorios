977283eb3aee6d0ac6410eac559091d367c6a384
Andrey <ajedrezandrey@gmail.com>
2022-11-17 21:11:15 -0500

    Actulizacion

--- Frontend/FrontendAccesorios/src/app/modulos/administracion/administracion-routing.module.ts
+++ Frontend/FrontendAccesorios/src/app/modulos/administracion/administracion-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { BuscarPersonaComponent } from './persona/buscar-persona/buscar-persona.component';
 import { CrearPersonaComponent } from './persona/crear-persona/crear-persona.component';
 import { EditarPersonaComponent } from './persona/editar-persona/editar-persona.component';
-<<<<<<< HEAD
 import { EliminarPersonaComponent } from './persona/eliminar-persona/eliminar-persona.component';
-=======
 import { BuscarProductoComponent } from './productos/buscar-producto/buscar-producto.component';
 import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
 import { EditarProductoComponent } from './productos/editar-producto/editar-producto.component';
 import { EliminarProductoComponent } from './productos/eliminar-producto/eliminar-producto.component';
->>>>>>> 10496ae41950b9bb44d9f3bd67a37fcda5476542
+
 
 const routes: Routes = [
   {
@@ -22,14 +20,16 @@ const routes: Routes = [
     component: EditarPersonaComponent
   },
   {
-<<<<<<< HEAD
+
     path: 'eliminar-persona/:id',
     component: EliminarPersonaComponent
   },
   {
     path: 'listar-persona',
     component:BuscarPersonaComponent
-=======
+  },
+  {
+    
     path: "listar-productos",
     component: BuscarProductoComponent
   },
@@ -44,7 +44,6 @@ const routes: Routes = [
   {
     path: 'eliminar-producto',
     component: EliminarProductoComponent
->>>>>>> 10496ae41950b9bb44d9f3bd67a37fcda5476542
   }
   
 
