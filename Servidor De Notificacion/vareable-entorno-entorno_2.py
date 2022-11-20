35c3a8e2d523c72382c945a6ec2c390076f268bf
Johan Andrey Palencia Ramirez <112025660+AndreyPalencia@users.noreply.github.com>
2022-11-20 15:55:29 -0500

    Update vareable-entorno-entorno_2.py

--- Servidor De Notificacion/vareable-entorno-entorno_2.py
+++ Servidor De Notificacion/vareable-entorno-entorno_2.py
@@ -1,17 +1,1 @@
-# -*- coding: utf-8 -*-
-#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
-#SG.6Ze0b3dkQnqBNncRBvmDhQ.YP0xxnUl6eDwHFmhWFe-_K5NUvQsQxfN9BxkPB5VUEek
-"""
-Created on Wed Oct 19 19:06:39 2022
 
-@author: Usuario
-"""
-
-import os 
-
-os.environ["TWILIO_ACCOUNT_SID"] = "ACccd527b2d3b7eb1f43d46808893e63e2"
-os.environ["TWILIO_AUTH_TOKEN"] = "29cc0297c1a12b4c93452a60d5d279cb"
-os.environ["SENDGRID_API_KEY"] = "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"
-os.environ["Test"]= "Prueba de variable de entorno"
-os.environ["Correo"] = "Este es para recibir correo y en viar mensajes gracias"
-os.environ["Autoidentificacion-correo"] = "ajedrezandrey@gmail.com"
